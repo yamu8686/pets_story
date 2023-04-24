6d3cfb9bb113d4e7e8ae5ac3f53d6c4cbafa2752
yamu8686 <117561782+yamu8686@users.noreply.github.com>
2023-04-24 06:51:19 +0000

    退会処理判別

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -2,6 +2,7 @@
 
 class Public::SessionsController < Devise::SessionsController
   # before_action :configure_sign_in_params, only: [:create]
+   before_action :user_state, only: [:create]
 
   # GET /resource/sign_in
   # def new
@@ -18,7 +19,20 @@ class Public::SessionsController < Devise::SessionsController
   #   super
   # end
 
-  # protected
+  protected
+  # 退会しているか判断するメソッド
+  def user_state
+    # [処理内容1]  入力されたemailからアカウントを１件取得
+    @user = User.find_by(email: params[:user][:email])
+    # アカウントを取得できなかった場合、このメソッドを終了する
+    return if !@user
+    # [処理内容２]  取得したアカウントのパスワードと入力されたパスワードが一致しているか判別
+    if @user.valid_password?(params[:user][:password]) && (@user.is_deleted == true)
+      # [処理内容３]
+      redirect_to new_user_session_path
+
+    end
+  end
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_in_params
