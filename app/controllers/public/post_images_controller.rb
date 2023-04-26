602beb3755e1d9ef7dc97696e58e8c25e7b7de86
yamu8686 <117561782+yamu8686@users.noreply.github.com>
2023-04-26 08:11:01 +0000

    削除機能

--- app/controllers/public/post_images_controller.rb
+++ app/controllers/public/post_images_controller.rb
@@ -22,6 +22,9 @@ class Public::PostImagesController < ApplicationController
   end
 
   def destroy
+    post_image = PostImage.find(params[:id])
+    post_image.destroy
+    redirect_to public_post_images_path
   end
 
   private
