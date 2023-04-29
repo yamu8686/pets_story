7f9a76bbc733e7724c5b3ce0e301ee8f822fa24f
yamu8686 <117561782+yamu8686@users.noreply.github.com>
2023-04-29 10:02:19 +0000

    コメント機能

--- app/controllers/public/post_comments_controller.rb
+++ app/controllers/public/post_comments_controller.rb
@@ -1,10 +1,11 @@
 class Public::PostCommentsController < ApplicationController
+  
   def create
     post_image = PostImage.find(params[:post_image_id])
     comment = current_user.post_comments.new(post_comment_params)
     comment.post_image_id = post_image.id
     comment.save
-    redirect_to post_image_path(post_image)
+    redirect_to public_post_image_path(post_image)
   end
 
   private
--- app/models/post_image.rb
+++ app/models/post_image.rb
@@ -2,13 +2,14 @@ class PostImage < ApplicationRecord
 
   has_one_attached :image
   belongs_to :user
+  has_many :post_comments, dependent: :destroy
   #has_many :post_comments, dependent:destroy
 
-  def get_image
+  def get_image(width,height)
     unless image.attached?
       file_path = Rails.root.join('app/assets/images/no_image.jpg')
       image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
     end
-    image
+    image.variant(resize_to_limit: [width,height]).processed
   end
 end
