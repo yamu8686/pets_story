dc4a9afa5d26d52326cc6fe081f08d10c5d71b0f
yamu8686 <117561782+yamu8686@users.noreply.github.com>
2023-05-06 06:39:19 +0000

    編集完了

--- app/controllers/public/post_images_controller.rb
+++ app/controllers/public/post_images_controller.rb
@@ -28,7 +28,7 @@ class Public::PostImagesController < ApplicationController
   def update
     @post_image = PostImage.find(params[:id])
     @post_image.update(post_image_params)
-    redirect_to public_post_image_path(post_image.id)
+    redirect_to public_post_image_path(@post_image.id)
   end
 
   def destroy
