06475f59ab983e57e922c3b1ae5ef9eaf5094504
yamu8686 <117561782+yamu8686@users.noreply.github.com>
2023-05-19 07:30:42 +0000

    投稿画像サイズ変更

--- app/models/post_image.rb
+++ app/models/post_image.rb
@@ -6,12 +6,12 @@ class PostImage < ApplicationRecord
   has_many :favorites, dependent: :destroy
   has_many :favorited_users, through: :favorites, source: :user
 
-  def get_image
+  def get_image(width, height)
     unless image.attached?
       file_path = Rails.root.join('app/assets/images/no_image.jpg')
       image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
     end
-    image
+    image.variant(resize_to_limit: [width,height]).processed
   end
 
   def favorited_by?(user)
